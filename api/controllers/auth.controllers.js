08d4110fee44e20bbb6c7d459cac6ccbdd122b2c
im-Maryam-Mahmood <159541999+im-Maryam-Mahmood@users.noreply.github.com>
2024-07-24 03:10:21 +0500

    Middleware & Function for Possible Errors

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -7,7 +7,7 @@ export const signup = async (req, res, next) => {
     try {
         await newUser.save();
         res.status(201).json('User created successfully!')
-    } catch (error) {
+    } catch (error) { 
         next(error);
     }
    
