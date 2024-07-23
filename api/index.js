d9193cd52f524e9a4bcbcc4f5f771194aac2473b
im-Maryam-Mahmood <159541999+im-Maryam-Mahmood@users.noreply.github.com>
2024-07-23 16:51:47 +0500

    Connect to Database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express()
 
-app.listen(3000, () =>{
-    console.log(`Server Started at Port : 3000`)
-})
+const MONGO = process.env.MONGO
+mongoose.connect(MONGO)
+    .then(() => {
+        console.log('MongoDB Connected');
+        app.listen(3000, () => {
+            console.log('Server Started at Port : 3000')
+        });
+    }).catch((err) => {
+        console.log(err)
+    });
+
+
+
