a0ffa2240abd1c0590188082e52749ac952fb7dd
im-Maryam-Mahmood <maryammahmoodpersonal@gmail.com>
2024-09-21 00:27:27 +0500

    Add Redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import { Link , useNavigate} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux'
+import {signInStart, signInSuccess, signInFailure} from  '../redux/user/userSlice';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const{error, loading } = useSelector((state) => state.user);
+
   const  navigate = useNavigate();
+  const dispatch = useDispatch();
 
 
   const handleChange = (e) => {
@@ -19,7 +22,7 @@ export default function SignIn() {
     e.preventDefault();
     
     try {
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -30,16 +33,13 @@ export default function SignIn() {
       const data = await res.json();
       // console.log(data);
       if(data.success === false){
-        setLoading(false)
-        setError(data.message)
+        dispatch(signInFailure())
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess())
       navigate('/')
     } catch (error) {
-      setLoading(false)
-      setError(error.message)
+      dispatch(signInFailure(error.message))
     }
   };
 
